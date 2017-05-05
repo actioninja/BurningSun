ebb01f54349309c4d0e69375eeaf99a82094c095
actioninja <actioninja@gmail.com>
2017-05-04 22:45:49 -0700

    Update config comments

--- src/main/java/actioninja/burningsun/Config.java
+++ src/main/java/actioninja/burningsun/Config.java
@@ -16,9 +16,9 @@ public class Config {
             public static int hyperLethalDamage = 10;
             @net.minecraftforge.common.config.Config.Comment({"Makes helmets block sunlight, like how they work on zombies and skeletons"})
             public static boolean helmetsBlockSun = false;
-            @net.minecraftforge.common.config.Config.Comment({"Makes helmets take damage while blocking sunlight"})
+            @net.minecraftforge.common.config.Config.Comment({"Makes helmets take damage while blocking sunlight","This is a bit too fast right now."})
             public static boolean helmetsTakeDamage = true;
-            @net.minecraftforge.common.config.Config.Comment({"Acts as a multiplier to the previous setting"})
+            @net.minecraftforge.common.config.Config.Comment({"Acts as a multiplier to the previous setting", "Integers only."})
             public static int helmetDamageMultiplier = 1;
 
         }
